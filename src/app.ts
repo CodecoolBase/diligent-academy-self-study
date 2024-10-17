b619daf8299fb19706115d2fc66e60894616c9a5
Daniel Hatas <21317298+samson84@users.noreply.github.com>
2024-10-17 13:56:07 +0200

    Create tasks, add type provider option.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import fastify from 'fastify';
+import { JsonSchemaToTsProvider } from '@fastify/type-provider-json-schema-to-ts'
 
 export default function createApp(options = {}) {
+  // App without type provider, use this one for separate type declarations
   const app = fastify(options)
 
+  // App with type provider, use this one for type providers
+  // const app = fastify(options).withTypeProvider<JsonSchemaToTsProvider>()
+
+
   app.get('/api/hello', (request, reply) => {
-    reply.send({hello: "World!"})
+    reply.send({ hello: "World!" })
   })
 
+  
   return app;
 }
